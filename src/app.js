9309ffdc31d07b2ac46c81e9939a1691031c31a4
Luka Piplica <piplicaluka64@gmail.com>
2024-12-30 22:48:02 -0500

    REPL added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 import { Lexer } from "./lexer/lexer.js";
 
-(function() {
-	const input = `{
-		key: "value"
-	}
-	`
+process.stdin.on("connection", () => {
+	console.log("JSON REPL Started");
+	console.log("Press ctrl+c or type .exit to quit the REPL")
+})
 
-	const lexer = new Lexer(input);
-	console.log(`current token = ${lexer.ch}`);
-	console.log(`peek token = ${lexer.peek().charCodeAt()}`);
-})();
+process.stdin.on("data", data => {
+	const dataStr = data.toString(undefined, 0, data.length - 1);
+
+	if (dataStr === ".exit") {
+		process.exit(0);
+	}
+
+	const l = new Lexer(dataStr);
+
+	while (1) {
+		const token = l.nextToken();
+		console.log(`${token.type}: ${token.literal}`);
+
+		if (token.type === "EOF") {
+			break;
+		}
+	}
+
+	console.log("\n");
+});
